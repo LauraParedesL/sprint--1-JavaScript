f0aa98e36bffeecc2557c24f1144b197e5bf9935
Laura Paredes <lauraparedesl35@outlook.com>
2023-11-13 18:01:23 -0500

    sprint3 finished

---
+++ js/functions.js
@@ -0,0 +1,46 @@
+export function introducirCards(array , contenedor){
+    for (const objetos of array) {
+        contenedor.innerHTML += crearCards(objetos)
+    }
+}
+
+export function crearCards(objeto){
+    return `<article class="flex flex-col items-center w-64 h-78 p-2.5 border-2 bg-amber-700 rounded-lg ">
+                   <img class="w-42 object-contain h-40 p-2" src="${objeto.image}">
+                   <h3 class="font-sans font-bold text-2xl text-white text-center">${objeto.title}</h3>
+                   </br>
+                    <p class="text-center font-serif text-white p-2 line-clamp-4 hover:block">${objeto.overview}</p>
+                    <a class="border-black bg-black text-white p-2 w-18 m-1 font-bold self-center rounded text-center" href="./details.html?id=${objeto.id}"> Ir a detalle </a>
+            </article>`
+}
+
+export function crearBuscarXGeneros(genreParameter){
+    return `<option value=${genreParameter}>${genreParameter}</option>`
+}
+
+export function imprimirTemplate(lista , ubicacion , fn){
+    let template= ""
+    for (const elementoIterado of lista) {
+        template += fn(elementoIterado)
+    }
+    if (template) {
+        ubicacion.innerHTML = template
+    }
+    else{
+        ubicacion.innerHTML = `<h2 class="text-black"> No hay resultados... </h2>`
+    }
+    
+}
+
+export function filteringByTitle(listaMovies , titulo){
+    const filtro= listaMovies.filter(movie => movie.title.toLowerCase().startsWith( titulo.toLowerCase() ) )
+    return filtro
+}
+
+export function filtrarPorGenero(listaMovies , genre){
+    const filtrado= listaMovies.filter(movie => genre.includes(...movie.genres ))
+    if(genre == "Genres"){
+        return listaMovies
+    }
+    return filtrado
+}
--- js/movies.js
+++ js/movies.js
@@ -1,4 +1,53 @@
+import { introducirCards, crearCards, crearBuscarXGeneros, imprimirTemplate, filteringByTitle, filtrarPorGenero } from "./functions.js"
+
 const contenedor=document.getElementById("cards")
+
+
+
+let section=document.querySelector("section")
+
+
+introducirCards(movies, contenedor)
+
+
+//filtros
+
+const inputBusqueda=document.getElementById("input-busqueda")
+const contenedorGeneros=document.getElementById("contenedor-generos")
+
+const generosSinFiltrar = movies.map(movie => movie.genres).flat()
+const generosSet= [...new Set(generosSinFiltrar)]
+
+const genres = Array.from(generosSet)
+genres.unshift("Genres")
+
+//genres.add("Genres")
+
+
+
+imprimirTemplate(genres, contenedorGeneros , crearBuscarXGeneros)
+
+inputBusqueda.addEventListener("keyup" , () => {
+    const filtroGeneros=contenedorGeneros.value
+    const filtradoPorGenero= filtrarPorGenero(movies, filtroGeneros)
+    const filtradoPorNombre= filteringByTitle(filtradoPorGenero, inputBusqueda.value)
+    imprimirTemplate(filtradoPorNombre, contenedor, crearCards)
+})
+
+contenedorGeneros.addEventListener("input",(e)=>{
+    const filtroGeneros=contenedorGeneros.value
+    const filtradoPorGenero= filtrarPorGenero(movies, filtroGeneros)
+    const filtradoPorNombre= filteringByTitle(filtradoPorGenero, inputBusqueda.value)
+    imprimirTemplate(filtradoPorNombre, contenedor, crearCards)
+})
+/*
+function filtrarPorGenero(listaMovies , genre){
+    const filtrado= listaMovies.filter(movie => movie.genres.includes(genre))
+    if(genre == "Genres"){
+        return listaMovies
+    }
+    return filtrado
+}
 
 function crearCards(objeto){
     return `<article class="flex flex-col items-center w-64 h-78 p-2.5 border-2 bg-amber-700 rounded-lg ">
@@ -6,35 +55,18 @@ function crearCards(objeto){
                    <h3 class="font-sans font-bold text-2xl text-white text-center">${objeto.title}</h3>
                    </br>
                     <p class="text-center font-serif text-white p-2 line-clamp-4 hover:block">${objeto.overview}</p>
+                    <a class="border-black bg-black text-white p-2 w-18 m-1 font-bold self-center rounded text-center" href="./details.html?id=${objeto.id}"> Ir a detalle </a>
             </article>`
 }
-
-let section=document.querySelector("section")
 
 function introducirCards(array){
     for (const objetos of array) {
         contenedor.innerHTML += crearCards(objetos)
     }
 }
-introducirCards(movies)
-
-//filtros
-
-const inputBusqueda=document.getElementById("input-busqueda")
-const contenedorGeneros=document.getElementById("contenedor-generos")
-
-let genres=new Set()
-
-for (const movie of movies) {
-    genres.add(...movie.genres)
-}
-
-console.log(...genres)
 
 function crearBuscarXGeneros(genreParameter){
-    return `<select name="genre" id="genres">
-                <option value=${genreParameter}>${genreParameter}</option>
-            </select>`
+    return `<option "value"=${genreParameter}>${genreParameter}</option>`
 }
 
 function imprimirTemplate(lista , ubicacion , fn){
@@ -50,17 +82,11 @@ function imprimirTemplate(lista , ubicacion , fn){
     }
     
 }
-imprimirTemplate(genres, contenedorGeneros , crearBuscarXGeneros)
-
-inputBusqueda.addEventListener("keyup" , () => {
-    const filterByTitle = filteringByTitle(movies , inputBusqueda.value)
-
-    imprimirTemplate(filterByTitle, contenedor, crearCards)
-})
-
 
 
 function filteringByTitle(listaMovies , titulo){
     const filtro= listaMovies.filter(movie => movie.title.toLowerCase().startsWith( titulo.toLowerCase() ) )
     return filtro
 }
+*/
+
